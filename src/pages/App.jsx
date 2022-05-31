f5d17b89a556d6833f4a21095b7946a1cd34769b
HanYS <hys6078@naver.com>
2022-06-01 00:51:46 +0900

    feat: 메인페이지 마우스 스크롤 이벤트 추가

---
+++ src/pages/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import MainPage from "./MainPage";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useRef, useState } from "react";
+import "./style.scss";
+
+import { Dots } from "../../components";
+
+const MainPage = () => {
+  const [scrollIdx, setScrollIdx] = useState(1);
+  const mainWrapperRef = useRef();
+
+  useEffect(() => {
+    const wheelHandler = (e) => {
+      console.log("wheel");
+      e.preventDefault();
+      const { deltaY } = e;
+      const { scrollTop } = mainWrapperRef.current; // 스크롤 위쪽 끝부분 위치
+      const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+
+      if (deltaY > 0) {
+        // 스크롤 내릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          mainWrapperRef.current.scrollTo({
+            top: pageHeight,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(2);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          mainWrapperRef.current.scrollTo({
+            top: pageHeight * 2,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(3);
+        } else {
+          // 현재 3페이지
+          mainWrapperRef.current.scrollTo({
+            top: pageHeight * 2,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(3);
+        }
+      } else {
+        // 스크롤 올릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //현재 1페이지
+          mainWrapperRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(1);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //현재 2페이지
+          mainWrapperRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(1);
+        } else {
+          // 현재 3페이지
+          mainWrapperRef.current.scrollTo({
+            top: pageHeight,
+            left: 0,
+            behavior: "smooth",
+          });
+          setScrollIdx(2);
+        }
+      }
+    };
+
+    const wrapperRefCurrent = mainWrapperRef.current;
+    wrapperRefCurrent.addEventListener("wheel", wheelHandler);
+
+    return () => {
+      //wrapperRefCurrent.removeEventHandler("wheel", wheelHandler);
+    };
+  }, []);
+
+  return (
+    <div className="main-wrapper" ref={mainWrapperRef}>
+      <Dots scrollIdx={scrollIdx} />
+      <div className="main-item">hi</div>
+      <div className="main-item">hi2</div>
+      <div className="main-item">hi3</div>
+    </div>
+  );
+};
+
+export default MainPage;
